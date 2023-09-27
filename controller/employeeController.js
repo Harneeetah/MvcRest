41d94fe2ec72b2dda53a865c581144e7a5565ac3
harneeetah <harneeetah@gmail.com>
2023-09-27 15:15:31 +0100

    controller

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -7,7 +7,7 @@ const getAllEmployees = (req, res) => {
     res.json(data.employees);
 }
 
-const createNewEmployee = (req, res) => {
+const createEmployee = (req, res) => {
     const newEmployee = {
         id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
         firstname: req.body.firstname,
